dd034c7561a1d62ea2922b41b10c47438759ba23
yaim0425 <yaim0425@gmail.com>
2025-06-22 11:21:50 -0500

    Creación de la función This_MOD.load_setting

--- data.lua
+++ data.lua
@@ -30,7 +30,25 @@ end
 
 ---------------------------------------------------------------------------------------------------
 
+--- Darle el formato deseado a las opciones
+--- de configuración de los mods
+function This_MOD.load_setting()
+    --- Inicializar el contenedor
+    GPrefix.Setting = {}
 
+    --- Recorrer las opciones de configuración
+    for key, value in pairs(settings.startup) do
+        --- Separar los datos esperados
+        local prefix = GPrefix.name .. "%-"
+        local id, name = key:match(prefix .. "(%d+)%-(.+)")
+
+        --- Validar los datos obtenidos
+        if id and name then
+            GPrefix.Setting[id] = GPrefix.Setting[id] or {}
+            GPrefix.Setting[id][name] = value
+        end
+    end
+end
 
 ---------------------------------------------------------------------------------------------------
 
