dd1e721eb876a63011ae5e16dbfefbd25f17ec7e
yaim0425 <yaim0425@gmail.com>
2025-09-27 17:23:30 -0500

    Creación de la variable GMOD.signal y eliminación de la variable GMOD.color

--- __CONSTANTS__.lua
+++ __CONSTANTS__.lua
@@ -67,17 +67,12 @@ _G.mods = _G.mods or {}
 --- Subgrupos existentes, se usa con frecuencia
 GMOD.subgroups = data.raw["item-subgroup"]
 
---- Colores para el fondo de los indicadres
-GMOD.color = {
-    black = data.raw["virtual-signal"]["signal-black"].icon,
-    blue = data.raw["virtual-signal"]["signal-blue"].icon,
-    cyan = data.raw["virtual-signal"]["signal-cyan"].icon,
-    green = data.raw["virtual-signal"]["signal-green"].icon,
-    grey = data.raw["virtual-signal"]["signal-grey"].icon,
-    pink = data.raw["virtual-signal"]["signal-pink"].icon,
-    red = data.raw["virtual-signal"]["signal-red"].icon,
-    white = data.raw["virtual-signal"]["signal-white"].icon,
-    yellow = data.raw["virtual-signal"]["signal-yellow"].icon,
-}
+--- Señales virtuales existentes, se usa con frecuencia
+GMOD.signal = {}
+for key, value in pairs(data.raw["virtual-signal"]) do
+    key = key:gsub("signal%-", "")
+    key = key:gsub("shape%-", "")
+    GMOD.signal[key] = value.icon
+end
 
 ---------------------------------------------------------------------------------------------------
